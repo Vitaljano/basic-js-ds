018a8d0c8882fe4e960060bd5c1f4ff4d3f7250e
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2022-04-29 23:29:43 +0300

    feat: done queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(element) {
+    let node = new ListNode(element);
+
+    let current;
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+}
+class Queue {
+  queue = [];
+
+  getUnderlyingList() {
+    let list = new LinkedList();
+
+    for (let value of this.queue) {
+      list.add(value);
+    }
+    return list.head;
+  }
+
+  enqueue(value) {
+    this.queue.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue.shift();
   }
 }
 
+// const queue = new Queue();
+// queue.enqueue(2);
+// queue.enqueue(3);
+// queue.enqueue(5);
+// console.log(queue.getUnderlyingList());
+
+// queue.enqueue(5);
+// queue.enqueue(6);
+// queue.enqueue(7);
+// console.log(queue.dequeue());
+// console.log(queue);
+// console.log(queue);
 module.exports = {
-  Queue
+  Queue,
 };
